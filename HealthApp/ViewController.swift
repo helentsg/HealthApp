0aadc0356dbd0f800b5f679dfab57ed85f2496af
Elena Tsegelnik <helentsg@yandex.ru>
2020-06-25 21:44:49 +0500

    Initial Commit

---
+++ HealthApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthApp
+//
+//  Created by Elena Tsegelnik on 25.06.2020.
+//  Copyright © 2020 Elena Tsegelnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
